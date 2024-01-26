1c82eb70a83d6c8cea0000271fe401b0305bb53d
Mateusz Rostkowski <mateusz.rostkowsky995@gmail.com>
2024-01-26 17:24:55 +0100

    chore: remove unused import and import Text from custom component

--- src/components/atoms/FormLabel.tsx
+++ src/components/atoms/FormLabel.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 
+import { Text } from './Text'
 import { FormLabelProps } from './types'
 
 import { useColorScheme } from '~contexts'
