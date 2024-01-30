b36366a2df438b8704b49d08838d121ff3c202d9
Mateusz Rostkowski <mateusz.rostkowsky995@gmail.com>
2024-01-30 17:09:21 +0100

    fix: improve i18n types

--- src/types/i18next.d.ts
+++ src/types/i18next.d.ts
@@ -1,7 +1,14 @@
 import 'i18next'
+import english from '~i18n/translations/en.json'
+
+type EN = typeof english
 
 declare module 'i18next' {
   interface CustomTypeOptions {
+    defaultNS: 'en'
     returnNull: false
+    resources: {
+      en: EN
+    }
   }
 }
