fa1478ba59b40165b8b80eb1f864d1c7713d7dd0
Mateusz Rostkowski <mateusz.rostkowsky995@gmail.com>
2024-02-22 14:26:33 +0100

    chore: change header on blog screen

--- src/screens/BlogScreen.tsx
+++ src/screens/BlogScreen.tsx
@@ -1,17 +1,13 @@
 import { Center, Text } from '~components'
-import { useScreenOptions, useTranslation } from '~hooks'
+import { LandingHeader } from '~components/LandingHeader'
 
 export const BlogScreen = () => {
-  const { t } = useTranslation()
-
-  useScreenOptions({
-    // TODO: Add translation
-    title: t('navigation.screen_titles.blog'),
-  })
-
   return (
-    <Center flex={1} px={4}>
-      <Text>This is blog screen, and is visible by logged in and logged out users</Text>
-    </Center>
+    <>
+      <LandingHeader />
+      <Center flex={1} px={4}>
+        <Text>This is blog screen, and is visible by logged in and logged out users</Text>
+      </Center>
+    </>
   )
 }
