94f46b27fbe33283a2b43c146700c6fc07b2671c
Bartłomiej Sworzeń <b.sworzen@gmail.com>
2024-03-09 12:26:36 +0100

    add regex for password

--- src/constants/regex.ts
+++ src/constants/regex.ts
@@ -1,4 +1,5 @@
 export const REGEX = {
   EMAIL: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/g,
-  REGISTRATION_PASSWORD: /(?=.*\d)(?=.*[A-Z])/g,
+  // PASSWORD - minimum: at least 8 chars, 1 small letter, 1 big letter, 1 special char, 1 number
+  REGISTRATION_PASSWORD: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/g,
 } as const
