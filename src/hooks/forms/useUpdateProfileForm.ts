00867d7618dd64b160f6d4bc2df3dd5e67053924
Mateusz Rostkowski <mateusz.rostkowsky995@gmail.com>
2024-03-11 20:08:48 +0100

    chore: unify user profile form

--- src/hooks/forms/useUpdateProfileForm.ts
+++ src/hooks/forms/useUpdateProfileForm.ts
@@ -1,14 +1,16 @@
 import { useAuthControllerUpdate, useAuthControllerMe } from '@baca/api/query/auth/auth'
 import { AuthUpdateDto } from '@baca/api/types'
-// import { AuthUpdateDto } from '@baca/types/profileForm'
 import { hapticImpact } from '@baca/utils'
 import { handleFormError } from '@baca/utils/handleFormErrors'
 import { useMemo, useEffect } from 'react'
 import { useForm } from 'react-hook-form'
+import { useTranslation } from 'react-i18next'
+import { notify } from 'react-native-notificated'
 
 export const useUpdateProfileForm = () => {
   const { data: userData } = useAuthControllerMe()
-  const { mutate: userUpdate, isLoading: isSubmitting } = useAuthControllerUpdate()
+  const { t } = useTranslation()
+  const { mutate: updateUserMutation, isLoading } = useAuthControllerUpdate()
 
   const defaultValues: AuthUpdateDto = useMemo(
     () => ({
@@ -22,21 +24,32 @@ export const useUpdateProfileForm = () => {
   const {
     control,
     formState: { errors },
-    setFocus,
-    reset,
     handleSubmit,
+    reset,
     setError: setFormError,
+    setFocus,
   } = useForm<AuthUpdateDto>({
     mode: 'onTouched',
     defaultValues,
   })
 
-  useEffect(() => reset(defaultValues), [reset, defaultValues])
+  useEffect(() => {
+    reset(defaultValues)
+  }, [reset, defaultValues])
 
   const onSubmit = async (data: AuthUpdateDto) => {
-    userUpdate(
+    await updateUserMutation(
       { data },
       {
+        onSuccess: () => {
+          notify('success', {
+            params: {
+              style: { multiline: 100 },
+              title: 'SUCCESS',
+              description: t('profile_screen.update_profile_success'),
+            },
+          })
+        },
         onError: (e) => {
           handleFormError<keyof AuthUpdateDto>(
             e as unknown as keyof AuthUpdateDto,
@@ -54,7 +67,7 @@ export const useUpdateProfileForm = () => {
   return {
     control,
     errors,
-    isSubmitting,
+    isSubmitting: isLoading,
     setFocus,
     submit: handleSubmit(onSubmit),
   }
