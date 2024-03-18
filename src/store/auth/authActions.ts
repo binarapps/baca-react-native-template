1c19598310c3e08dfc0669e48c724c257ab62a86
Mateusz Rostkowski <mateusz.rostkowsky995@gmail.com>
2024-03-18 20:07:05 +0100

    feat: remove push token when siging out

--- src/store/auth/authActions.ts
+++ src/store/auth/authActions.ts
@@ -1,9 +1,24 @@
+import { ASYNC_STORAGE_KEYS } from '@baca/constants'
 import { deleteToken } from '@baca/services'
 import { store } from '@baca/store/store'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 import { isSignedInAtom } from './authState'
 
 export async function signOut() {
-  await deleteToken()
+  // set user logged out
   store.set(isSignedInAtom, false)
+
+  // remove auth token from device
+  await deleteToken()
+
+  // remove push token from backend
+  const pushTokenStorage = await AsyncStorage.getItem(ASYNC_STORAGE_KEYS.PUSH_TOKEN)
+
+  if (pushTokenStorage) {
+    // TODO: Remove push token from backend
+    console.log('REMOVE ME from BACKEND', pushTokenStorage)
+  }
+
+  await AsyncStorage.setItem(ASYNC_STORAGE_KEYS.WAS_PUSH_TOKEN_SEND, 'false')
 }
