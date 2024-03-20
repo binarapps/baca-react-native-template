240aa9340579e8bf800e462b06f8de13e7c258b9
Mateusz Rostkowski <mateusz.rostkowsky995@gmail.com>
2024-03-20 10:08:48 +0100

    chore: move EAS_PROJECT_ID to envs

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -3,4 +3,5 @@ import Constants from 'expo-constants'
 export const ENV = {
   API_URL: Constants?.expoConfig?.extra?.API_URL,
   ENVIRONMENT: Constants?.expoConfig?.extra?.ENVIRONMENT,
+  EAS_PROJECT_ID: 'ac562c27-4a4e-4532-869f-fe6f9447bee6', // FIXME: Move it to .env
 }
