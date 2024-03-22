72f04de07996c4a9228aa77735191e5abcc927bc
Michał Baumruck <micbaumr@gmail.com>
2024-03-22 12:00:21 +0100

    replace company logo here and there

--- src/screens/CategoriesScreen.tsx
+++ src/screens/CategoriesScreen.tsx
@@ -1,32 +1,16 @@
-import { darkLogo, lightLogo } from '@baca/constants'
-import { useColorScheme } from '@baca/contexts'
+import { CompanyLogo } from '@baca/components'
 import { Center, Text } from '@baca/design-system'
 import { useScreenOptions } from '@baca/hooks'
-import { Image, StyleSheet } from 'react-native'
 
 export const CategoriesScreen = () => {
   useScreenOptions({
     title: 'Categories',
   })
 
-  const { colorScheme } = useColorScheme()
-
   return (
     <Center flex={1} px={4}>
-      <Image
-        resizeMethod="resize"
-        resizeMode="contain"
-        source={colorScheme === 'light' ? lightLogo : darkLogo}
-        style={styles.logo}
-      />
+      <CompanyLogo />
       <Text.LgBold textAlign="center">Categories screen</Text.LgBold>
     </Center>
   )
 }
-
-const styles = StyleSheet.create({
-  logo: {
-    height: 50,
-    width: '100%',
-  },
-})
--- src/screens/LandingScreen.tsx
+++ src/screens/LandingScreen.tsx
@@ -1,11 +1,7 @@
-import { KeyboardAwareScrollView } from '@baca/components'
-import { LandingHeader } from '@baca/components/LandingHeader'
-import { darkLogo, lightLogo } from '@baca/constants'
-import { useColorScheme } from '@baca/contexts'
+import { CompanyLogo, KeyboardAwareScrollView, LandingHeader } from '@baca/components'
 import { Button, Center, Text } from '@baca/design-system'
 import { useCallback, useScreenOptions, useTranslation } from '@baca/hooks'
 import { router } from 'expo-router'
-import { Image, StyleSheet } from 'react-native'
 
 export const LandingScreen = () => {
   const { t } = useTranslation()
@@ -13,8 +9,6 @@ export const LandingScreen = () => {
   useScreenOptions({
     title: t('navigation.screen_titles.home'),
   })
-
-  const { colorScheme } = useColorScheme()
 
   const navigateToBlog = useCallback(() => {
     router.navigate('/blog')
@@ -28,12 +22,7 @@ export const LandingScreen = () => {
     <KeyboardAwareScrollView>
       <LandingHeader />
       <Center flex={1} px={4}>
-        <Image
-          resizeMethod="resize"
-          resizeMode="contain"
-          source={colorScheme === 'light' ? lightLogo : darkLogo}
-          style={styles.logo}
-        />
+        <CompanyLogo />
         <Text.LgBold textAlign="center">{t('hello')}</Text.LgBold>
         <Text.MdRegular textAlign="center">{t('thanks')}</Text.MdRegular>
         <Text.MdRegular textAlign="center">{t('app_information')}</Text.MdRegular>
@@ -47,10 +36,3 @@ export const LandingScreen = () => {
     </KeyboardAwareScrollView>
   )
 }
-
-const styles = StyleSheet.create({
-  logo: {
-    height: 50,
-    width: '100%',
-  },
-})
