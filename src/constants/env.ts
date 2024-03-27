c82a4a20edb3530b8f585ef58c05a16c16aa2aab
Mateusz Rostkowski <mateusz.rostkowsky995@gmail.com>
2024-03-27 20:46:18 +0100

    chore: use eas project id from env variables

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -3,5 +3,5 @@ import Constants from 'expo-constants'
 export const ENV = {
   API_URL: Constants?.expoConfig?.extra?.API_URL,
   ENVIRONMENT_NAME: Constants?.expoConfig?.extra?.ENVIRONMENT_NAME,
-  EAS_PROJECT_ID: 'ac562c27-4a4e-4532-869f-fe6f9447bee6', // FIXME: Move it to .env
+  EAS_PROJECT_ID: Constants.expoConfig?.extra?.eas?.projectId,
 }
