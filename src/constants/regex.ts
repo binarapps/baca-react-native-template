26773e58c2a53675b9f9ffd8e5c1456699739c8a
Michał Baumruck <micbaumr@gmail.com>
2024-03-28 14:32:42 +0100

    add custom password validation

--- src/constants/regex.ts
+++ src/constants/regex.ts
@@ -2,4 +2,6 @@ export const REGEX = {
   EMAIL: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/g,
   // PASSWORD - minimum: at least 8 chars, 1 small letter, 1 big letter, 1 special char, 1 number
   REGISTRATION_PASSWORD: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&*!])[A-Za-z\d@#$%^&*!]{8,}$/,
+  MIN_8_CHARS: /^.{8,}$/,
+  SPECIAL_CHAR: /.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?].*/,
 } as const
