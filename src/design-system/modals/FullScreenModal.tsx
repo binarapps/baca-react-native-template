ef158dc4eae6e82ce1e03b37c70a44c6eaad8a78
Mateusz Rostkowski <mateusz.rostkowsky995@gmail.com>
2024-04-06 14:44:32 +0200

    feat: create full screen modal component

---
+++ src/design-system/modals/FullScreenModal.tsx
@@ -0,0 +1,37 @@
+import { useBoolean, useImperativeHandle } from '@baca/hooks'
+import { RefObject } from 'react'
+import { Modal, ModalProps } from 'react-native'
+
+import { Box } from '../components'
+
+export type ModalMethods = {
+  present: () => void
+  close: () => void
+}
+
+type Props = {
+  modalRef: RefObject<ModalMethods>
+} & ModalProps
+
+export const FullScreenModal = ({ children, modalRef, ...rest }: Props) => {
+  const [isOpen, setIsOpen] = useBoolean(false)
+
+  useImperativeHandle(modalRef, () => ({
+    present: setIsOpen.on,
+    close: setIsOpen.off,
+  }))
+
+  return (
+    <Modal
+      {...rest}
+      animationType="fade"
+      transparent
+      visible={isOpen}
+      onRequestClose={setIsOpen.off}
+    >
+      <Box bg="bg.brand.primary" w="100%" height="100%">
+        {children}
+      </Box>
+    </Modal>
+  )
+}
