76d616fc67bb0624ea500623f282d8f07497026c
Michał Baumruck <m.baumruck@binarapps.com>
2024-04-11 11:20:35 +0200

    add back button on not found screen

--- src/screens/NotFoundScreen.tsx
+++ src/screens/NotFoundScreen.tsx
@@ -1,5 +1,12 @@
-import { Center, Text } from '@baca/design-system'
+import { Box, Button, Text } from '@baca/design-system'
 import { useScreenOptions, useTranslation } from '@baca/hooks'
+import { router } from 'expo-router'
+import { StyleSheet } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const navigateToLogin = () => {
+  router.navigate('/sign-in')
+}
 
 export const NotFoundScreen = (): JSX.Element => {
   const { t } = useTranslation()
@@ -9,8 +16,15 @@ export const NotFoundScreen = (): JSX.Element => {
   })
 
   return (
-    <Center>
-      <Text>{t('errors.screen_not_found')}</Text>
-    </Center>
+    <SafeAreaView style={styles.safeArea}>
+      <Box alignItems="center" flexGrow={1} gap={8} p={8}>
+        <Text>{t('errors.screen_not_found')}</Text>
+        <Button maxW={360} onPress={navigateToLogin} title={t('common.continue')} w="full" />
+      </Box>
+    </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  safeArea: { flexGrow: 1 },
+})
