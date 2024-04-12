dba739cb57c33fd89045616606dfa25b90a547fa
Michał Baumruck <m.baumruck@binarapps.com>
2024-04-12 10:27:55 +0200

    Merge branch 'feat/google-sign-in-logic' into feat/facebook-sign-in

--- src/components/molecules/SocialButtons/FacebookButton/index.tsx
+++ src/components/molecules/SocialButtons/FacebookButton/index.tsx
@@ -3,7 +3,8 @@ import { isExpoGo, isWeb } from '@baca/constants'
 import { NativeFacebookButton } from './NativeFacebookButton'
 
 export const FacebookButton = () => {
-  if (isExpoGo || isWeb) return undefined //TODO: Add facebook button for web
+  //TODO: Add facebook button for web
+  if (isExpoGo || isWeb) return null
 
   return <NativeFacebookButton />
 }
