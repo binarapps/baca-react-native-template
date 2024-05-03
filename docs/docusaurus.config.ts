e6659ddd533c09d13628b0d8f9e4b95b047cb3b8
Mateusz Rostkowski <mateusz.rostkowsky995@gmail.com>
2024-05-03 14:12:21 +0200

    docs: add possibility to color lines in docs code

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -110,7 +110,25 @@ const config: Config = {
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
-      additionalLanguages: ['bash'],
+      additionalLanguages: ['bash', 'ruby', 'json', 'ejs', 'diff', 'markup-templating'],
+      magicComments: [
+        // Remember to extend the default highlight class name as well!
+        {
+          className: 'theme-code-block-highlighted-line',
+          line: 'highlight-next-line',
+          block: { start: 'highlight-start', end: 'highlight-end' },
+        },
+        {
+          className: 'code-block-error-line',
+          line: 'error-line',
+          block: { start: 'error-line-start', end: 'error-line-end' },
+        },
+        {
+          className: 'code-block-success-line',
+          line: 'success-line',
+          block: { start: 'success-line-start', end: 'success-line-end' },
+        },
+      ],
     },
   } satisfies Preset.ThemeConfig,
 }
