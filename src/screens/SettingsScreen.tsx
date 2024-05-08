381e133b17ac9165423e50c3be43addc4fbdd8ac
weronika <werawera112@gmail.com>
2024-05-08 15:30:41 +0200

    move remove account button to profile; remove unused translations

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,16 +1,14 @@
-import { useAuthControllerDelete } from '@baca/api/query/auth/auth'
 import { Version } from '@baca/components'
 import { colorSchemesList } from '@baca/constants'
 import { useColorScheme } from '@baca/contexts'
 import { Spacer, Button, Center, Text, ScrollView } from '@baca/design-system'
 import { useCallback, useScreenOptions, useTranslation } from '@baca/hooks'
 import { signOut } from '@baca/store/auth'
-import { alert, noop, showErrorToast } from '@baca/utils'
+import { noop } from '@baca/utils'
 
 export const SettingsScreen = (): JSX.Element => {
   const { t } = useTranslation()
   const { setColorSchemeSetting, colorSchemeSetting } = useColorScheme()
-  const { mutateAsync: removeUserAccount, isLoading } = useAuthControllerDelete()
 
   useScreenOptions({
     title: t('navigation.screen_titles.settings'),
@@ -22,28 +20,6 @@ export const SettingsScreen = (): JSX.Element => {
     },
     [setColorSchemeSetting]
   )
-
-  const handleRemoveUserAccount = useCallback(async () => {
-    alert(t('alert.remove_account.title'), t('alert.remove_account.description'), [
-      {
-        text: t('alert.remove_account.confirm'),
-        onPress: async () => {
-          try {
-            await removeUserAccount()
-            signOut()
-          } catch {
-            showErrorToast({
-              description: t('errors.something_went_wrong'),
-            })
-          }
-        },
-      },
-      {
-        text: t('common.cancel'),
-        style: 'destructive',
-      },
-    ])
-  }, [removeUserAccount, t])
 
   return (
     <ScrollView mt={4}>
@@ -64,9 +40,6 @@ export const SettingsScreen = (): JSX.Element => {
         <Button.SecondaryColor mt={8} size="lg" onPress={signOut}>
           {t('settings_screen.sign_out')}
         </Button.SecondaryColor>
-        <Button mt={8} onPress={handleRemoveUserAccount} loading={isLoading}>
-          {t('settings_screen.remove_account')}
-        </Button>
         <Spacer y={10} />
         <Version onPress={noop} />
       </Center>
