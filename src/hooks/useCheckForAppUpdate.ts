c0df1e55efff63c7bd5b48c00bda89d2560e7fd0
Olaf Chwołka <olaf.chwolka@outlook.com>
2024-05-20 09:03:17 +0200

    code optimizations

---
+++ src/hooks/useCheckForAppUpdate.ts
@@ -0,0 +1,50 @@
+import { useSystemControllerCheckForAppUpdate } from '@baca/api/query/system/system'
+import { useTranslation } from '@baca/hooks'
+import * as Application from 'expo-application'
+import { useEffect, useState } from 'react'
+import { Platform, Alert, Linking } from 'react-native'
+
+export const useCheckForAppUpdate = () => {
+  const { t } = useTranslation()
+  const currentVersion = Application.nativeApplicationVersion || 'unknown'
+  const os = Platform.OS
+  const APP_STORE_URL = `https://apps.apple.com/app/id`
+  const PLAY_STORE_URL = `https://play.google.com/store/apps/details?id=`
+  const [isUpdateLoading, setIsUpdateLoading] = useState<boolean>(true)
+
+  const { mutate: checkForUpdate } = useSystemControllerCheckForAppUpdate()
+
+  useEffect(() => {
+    if (os === 'ios' || os === 'android') {
+      checkForUpdate(
+        {
+          data: {
+            currentVersion,
+            os,
+          },
+        },
+        {
+          onSuccess: ({ updateRequired, appId }) => {
+            if (updateRequired) {
+              const storeUrl =
+                os === 'ios' ? `${APP_STORE_URL}${appId}` : `${PLAY_STORE_URL}${appId}`
+              Alert.alert(t('update.alert_title'), t('update.alert_message'), [
+                { text: t('update.update_now'), onPress: () => Linking.openURL(storeUrl) },
+              ])
+            }
+            setIsUpdateLoading(false)
+          },
+          onError: (error) => {
+            console.error('Error checking for updates:', error)
+            // CONFIG: Add log to your analytics service, for example - sentry o crashlytics
+            setIsUpdateLoading(false)
+          },
+        }
+      )
+    } else {
+      setIsUpdateLoading(false)
+    }
+  }, [os, currentVersion, checkForUpdate, t])
+
+  return isUpdateLoading
+}
