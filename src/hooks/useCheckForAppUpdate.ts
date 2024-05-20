ae635fb733dafc80212680d70cedff11f0f9decc
Olaf Chwołka <olaf.chwolka@outlook.com>
2024-05-20 12:33:48 +0200

    onSettled included

--- src/hooks/useCheckForAppUpdate.ts
+++ src/hooks/useCheckForAppUpdate.ts
@@ -3,18 +3,18 @@ import { useTranslation } from '@baca/hooks'
 import * as Application from 'expo-application'
 import { useEffect, useState } from 'react'
 import { Platform, Alert, Linking } from 'react-native'
+import { APP_STORE_URL, PLAY_STORE_URL } from 'scripts/cli/constants'
+
+const currentVersion = Application.nativeApplicationVersion || 'unknown'
+const { OS } = Platform
 
 export const useCheckForAppUpdate = () => {
   const { t } = useTranslation()
-  const currentVersion = Application.nativeApplicationVersion || 'unknown'
-  const { OS } = Platform
   const [isUpdateLoading, setIsUpdateLoading] = useState<boolean>(true)
 
   const { mutate: checkForUpdate } = useSystemControllerCheckForAppUpdate()
 
   useEffect(() => {
-    const APP_STORE_URL = `https://apps.apple.com/app/id`
-    const PLAY_STORE_URL = `https://play.google.com/store/apps/details?id=`
     if (OS === 'ios' || OS === 'android') {
       checkForUpdate(
         {
@@ -32,11 +32,12 @@ export const useCheckForAppUpdate = () => {
                 { text: t('update.update_now'), onPress: () => Linking.openURL(storeUrl) },
               ])
             }
-            setIsUpdateLoading(false)
           },
           onError: (error) => {
             console.error('Error checking for updates:', error)
             // CONFIG: Add log to your analytics service, for example - sentry o crashlytics
+          },
+          onSettled: () => {
             setIsUpdateLoading(false)
           },
         }
