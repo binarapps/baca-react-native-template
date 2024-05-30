f8503e0ac3c236f7bedb7e2b569874804099bf2c
Olaf Chwołka <olaf.chwolka@outlook.com>
2024-05-30 15:59:05 +0200

    ProfilePasswordForm working

--- src/components/screens/profile/ProfilePasswordForm.tsx
+++ src/components/screens/profile/ProfilePasswordForm.tsx
@@ -1,4 +1,3 @@
-import { AuthUpdateDto } from '@baca/api/types'
 import { Box, Button, Row } from '@baca/design-system'
 import { useUpdatePasswordForm } from '@baca/hooks/forms/useUpdatePasswordForm'
 import React from 'react'
@@ -6,11 +5,7 @@ import { useTranslation } from 'react-i18next'
 
 import { ProfileControlledInput } from './ProfileControlledInput'
 
-interface ProfilePasswordFormProps {
-  onSubmit: (data: AuthUpdateDto) => void
-}
-
-export const ProfilePasswordForm = ({ onSubmit }: ProfilePasswordFormProps) => {
+export const ProfilePasswordForm = () => {
   const { t } = useTranslation()
   const { control, errors, submit, isSubmitting } = useUpdatePasswordForm()
 
@@ -23,7 +18,6 @@ export const ProfilePasswordForm = ({ onSubmit }: ProfilePasswordFormProps) => {
           placeholder={t('form.placeholders.old_password')}
           control={control}
           errors={errors}
-          // secureTextEntry
         />
         <ProfileControlledInput
           label={t('form.labels.new_password')}
@@ -31,7 +25,6 @@ export const ProfilePasswordForm = ({ onSubmit }: ProfilePasswordFormProps) => {
           placeholder={t('form.placeholders.new_password')}
           control={control}
           errors={errors}
-          // secureTextEntry
         />
         <Row maxW={800} justifyContent="flex-end">
           <Button
@@ -47,89 +40,3 @@ export const ProfilePasswordForm = ({ onSubmit }: ProfilePasswordFormProps) => {
     </Box>
   )
 }
-
-// import { Box, Button } from '@baca/design-system'
-// import { useTranslation } from '@baca/hooks'
-// import { useUpdatePasswordForm } from '@baca/hooks/forms/useUpdatePasswordForm'
-// import { ProfileControlledInput } from './ProfileControlledInput'
-// import { AuthUpdateDto } from '@baca/api/types'
-
-// interface ProfilePasswordFormProps {
-//   onSubmit: (data: AuthUpdateDto) => void
-// }
-
-// export const ProfilePasswordForm = ({ onSubmit }: ProfilePasswordFormProps) => {
-//   const { t } = useTranslation()
-//   const { control, errors, isSubmitting, submit } = useUpdatePasswordForm()
-
-//   return (
-//     <Box
-//       /* as="form" onSubmit={submit}  */ borderColor="border.secondary"
-//       borderTopWidth={1}
-//       py={6}
-//     >
-//       <ProfileControlledInput
-//         label={t('form.labels.old_password')}
-//         name="oldPassword"
-//         placeholder={t('form.placeholders.old_password')}
-//         control={control}
-//         errors={errors}
-//         // isPassword
-//       />
-//       <ProfileControlledInput
-//         label={t('form.labels.new_password')}
-//         name="password"
-//         placeholder={t('form.placeholders.new_password')}
-//         control={control}
-//         errors={errors}
-//         // isPassword
-//       />
-//       <Button type="submit" isLoading={isSubmitting}>
-//         {t('common.change')}
-//       </Button>
-//     </Box>
-//   )
-// }
-
-// import { Box, Spacer, Button } from '@baca/design-system'
-// import { useForm, Controller } from 'react-hook-form'
-// import { ProfileControlledInput } from './ProfileControlledInput'
-// import { useTranslation } from '@baca/hooks'
-// import { AuthUpdateDto } from '@baca/api/types'
-
-// interface ProfilePasswordFormProps {
-//   onSubmit: (data: AuthUpdateDto) => void
-// }
-
-// export const ProfilePasswordForm = ({ onSubmit }: ProfilePasswordFormProps) => {
-//   const {
-//     control,
-//     handleSubmit,
-//     formState: { errors },
-//   } = useForm<AuthUpdateDto>()
-//   const { t } = useTranslation()
-
-//   return (
-//     <Box as="form" onSubmit={handleSubmit(onSubmit)}>
-//       <ProfileControlledInput
-//         label={t('form.labels.old_password')}
-//         name="oldPassword"
-//         placeholder={t('form.placeholders.old_password')}
-//         control={control}
-//         errors={errors}
-//         isPassword
-//       />
-//       <Spacer y={4} />
-//       <ProfileControlledInput
-//         label={t('form.labels.new_password')}
-//         name="password"
-//         placeholder={t('form.placeholders.new_password')}
-//         control={control}
-//         errors={errors}
-//         isPassword
-//       />
-//       <Spacer y={4} />
-//       <Button type="submit">{t('form.buttons.update_password')}</Button>
-//     </Box>
-//   )
-// }
--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,4 +1,3 @@
-import { AuthUpdateDto } from '@baca/api/types'
 import { ProfileDeleteAccountButton } from '@baca/components/screens/profile/ProfileDeleteAccountButton'
 import { ProfileDetailsForm } from '@baca/components/screens/profile/ProfileDetailsForm'
 import { ProfileHeader } from '@baca/components/screens/profile/ProfileHeader'
@@ -13,50 +12,15 @@ export const ProfileScreen = () => {
     title: t('navigation.screen_titles.profile'),
   })
 
-  const handlePasswordUpdate = (data: AuthUpdateDto) => {
-    console.log('Password updated:', data)
-  }
-
   return (
     <Box p={4}>
       <ProfileHeader />
       <Spacer y={4} />
       <ProfileDetailsForm />
       <Spacer y={4} />
-      <ProfilePasswordForm onSubmit={handlePasswordUpdate} />
+      <ProfilePasswordForm />
       <Spacer y={4} />
       <ProfileDeleteAccountButton />
     </Box>
   )
 }
-
-// import { ProfileDeleteAccountButton } from '@baca/components/screens/profile/ProfileDeleteAccountButton'
-// import { ProfileDetailsForm } from '@baca/components/screens/profile/ProfileDetailsForm'
-// import { ProfileHeader } from '@baca/components/screens/profile/ProfileHeader'
-// import { ProfilePasswordForm } from '@baca/components/screens/profile/ProfilePasswordForm'
-// import { Box, Spacer } from '@baca/design-system'
-// import { useTranslation, useScreenOptions } from '@baca/hooks'
-
-// export const ProfileScreen = () => {
-//   const { t } = useTranslation()
-
-//   useScreenOptions({
-//     title: t('navigation.screen_titles.profile'),
-//   })
-
-//   const handlePasswordUpdate = (data) => {
-//     console.log('Password updated:', data)
-//   }
-
-//   return (
-//     <Box p={4}>
-//       <ProfileHeader />
-//       <Spacer y={4} />
-//       <ProfileDetailsForm />
-//       <Spacer y={4} />
-//       <ProfilePasswordForm onSubmit={handlePasswordUpdate} />
-//       <Spacer y={4} />
-//       <ProfileDeleteAccountButton />
-//     </Box>
-//   )
-// }
