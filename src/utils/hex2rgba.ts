189ebd43c71da0052f7743146b2ae76c84771fc1
Mateusz Rostkowski <mateusz.rostkowsky995@gmail.com>
2024-05-30 20:17:38 +0200

    chore: add tests to hex2rgba function

--- src/utils/hex2rgba.ts
+++ src/utils/hex2rgba.ts
@@ -1,11 +1,20 @@
 export const hex2rgba = (hex: string, alpha = 0.75) => {
+  if (!hex || !hex.startsWith('#') || (hex.length !== 4 && hex.length !== 7)) {
+    return `rgba(0, 0, 0, ${alpha})`
+  }
   const newHex =
     hex.length === 4
       ? hex.split('').reduce((prev, curr) => {
           return prev + curr + curr
         })
       : hex
-  // @ts-expect-error: object is possibly null
-  const [r, g, b] = newHex.match(/\w\w/g).map((x) => parseInt(x, 16))
+
+  const hexRegex = newHex.match(/\w\w/g)
+
+  if (hexRegex?.some((x) => !x) || hexRegex?.length !== 3) {
+    return `rgba(0, 0, 0, ${alpha})`
+  }
+
+  const [r, g, b] = hexRegex?.map((x) => parseInt(x, 16)) ?? [0, 0, 0]
   return `rgba(${r},${g},${b},${alpha})`
 }
