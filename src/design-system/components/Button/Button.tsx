5f64e30210454a029315ee3abee2276028beba8a
Olaf Chwołka <olaf.chwolka@outlook.com>
2024-06-05 17:31:58 +0200

    types imports optimized

--- src/design-system/components/Button/Button.tsx
+++ src/design-system/components/Button/Button.tsx
@@ -1,5 +1,5 @@
 import { useTheme } from '@baca/hooks'
-import { IconNames } from '@baca/types/icon'
+import { IconNames } from '@baca/types'
 import { getColorValue } from '@baca/utils'
 import {
   useMemo,
