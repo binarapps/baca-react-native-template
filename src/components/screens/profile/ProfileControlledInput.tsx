d617680c76dbf5d906c38bbc6e6b29a4109a90fb
Olaf Chwołka <olaf.chwolka@outlook.com>
2024-06-05 17:24:53 +0200

    Rest operator on props and index.ts for types added

--- src/components/screens/profile/ProfileControlledInput.tsx
+++ src/components/screens/profile/ProfileControlledInput.tsx
@@ -2,18 +2,14 @@ import { ControlledField } from '@baca/components'
 import { isWeb } from '@baca/constants'
 import { Box, Text } from '@baca/design-system'
 import { useWeb } from '@baca/hooks'
-import { ProfileControlledInputProps } from '@baca/types/ProfileInputProps'
+import { ProfileControlledInputProps } from '@baca/types'
 import { StyleSheet } from 'react-native'
 
 export const ProfileControlledInput = ({
   label,
   name,
-  placeholder,
-  control,
-  errors,
   isDisabled = false,
-  onFocus,
-  onSubmitEditing,
+  ...rest
 }: ProfileControlledInputProps) => {
   const { shouldApplyMobileStyles } = useWeb()
 
@@ -29,16 +25,12 @@ export const ProfileControlledInput = ({
       </Text.SmBold>
       <Box flex={isWeb ? 2 : 0}>
         <ControlledField.Input
-          control={control}
-          errors={errors}
           autoCapitalize="none"
           inputMode={name === 'email' ? 'email' : 'text'}
           name={name}
-          placeholder={placeholder}
           testID={`${name}Input`}
           isDisabled={isDisabled}
-          onFocus={onFocus}
-          onSubmitEditing={onSubmitEditing}
+          {...rest}
         />
       </Box>
     </Box>
