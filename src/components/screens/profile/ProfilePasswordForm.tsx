5b721dc831b1476ae1b9673dcac73ecd9c7be2a2
Olaf Chwołka <olaf.chwolka@outlook.com>
2024-06-20 19:44:54 +0200

    password with validations done

--- src/components/screens/profile/ProfilePasswordForm.tsx
+++ src/components/screens/profile/ProfilePasswordForm.tsx
@@ -1,12 +1,19 @@
 import { Box, Button, Row } from '@baca/design-system'
-import { useUpdatePasswordForm } from '@baca/hooks/forms/useUpdatePasswordForm'
+import { useCallback } from '@baca/hooks'
+import { useUpdatePasswordForm } from '@baca/hooks/forms'
+import { usePasswordValidation } from '@baca/hooks/usePasswordValidation'
 import { useTranslation } from 'react-i18next'
+import { Keyboard } from 'react-native'
 
 import { ProfileControlledInput } from './ProfileControlledInput'
 
 export const ProfilePasswordForm = () => {
   const { t } = useTranslation()
-  const { control, errors, submit, isSubmitting } = useUpdatePasswordForm()
+  const { control, errors, submit, isSubmitting, setFocus } = useUpdatePasswordForm()
+
+  const { isPasswordError, passwordSuggestions, validationFn } = usePasswordValidation()
+
+  const focusNewPasswordInput = useCallback(() => setFocus('password'), [setFocus])
 
   return (
     <Box borderColor="border.secondary" borderTopWidth={1} py={6}>
@@ -16,14 +23,21 @@ export const ProfilePasswordForm = () => {
         placeholder={t('form.placeholders.old_password')}
         control={control}
         errors={errors}
+        onSubmitEditing={focusNewPasswordInput}
       />
       <ProfileControlledInput
         label={t('form.labels.new_password')}
         name="password"
         placeholder={t('form.placeholders.new_password')}
         control={control}
-        errors={errors}
+        errors={{}}
+        isInvalid={isPasswordError}
+        isRequired
+        rules={{ validate: { validationFn } }}
+        type="password"
+        onSubmitEditing={Keyboard.dismiss}
       />
+      {passwordSuggestions}
       <Row maxW={800} justifyContent="flex-end">
         <Button
           disabled={isSubmitting}
--- src/types/ProfileInputProps.ts
+++ src/types/ProfileInputProps.ts
@@ -1,12 +1,16 @@
-import { Control, FieldErrors } from 'react-hook-form'
+import { Control, FieldErrors, RegisterOptions } from 'react-hook-form'
 
 export type ProfileControlledInputProps = {
-  label: string
-  name: string
-  placeholder: string
   control: Control
   errors: FieldErrors
   isDisabled?: boolean
+  isInvalid?: boolean
+  isRequired?: boolean
+  label: string
+  name: string
   onFocus?: () => void
   onSubmitEditing?: () => void
+  placeholder: string
+  rules?: RegisterOptions
+  type?: 'text' | 'password' | undefined
 }
