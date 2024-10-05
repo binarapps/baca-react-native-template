61de9bc7c06df466f61e520948b88c81a6bc23c9
Mateusz Rostkowski <mateusz.rostkowsky995@gmail.com>
2024-10-05 13:15:55 +0200

    chore: change assets folder path

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,11 +1,11 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-export const darkLogo = require('@/assets/logo/logo-dark.png')
-export const lightLogo = require('@/assets/logo/logo-light.png')
+export const darkLogo = require('~/assets/logo/logo-dark.png')
+export const lightLogo = require('~/assets/logo/logo-light.png')
 
-export const darkBinarLogo = require('@/assets/logo/logo-dark.png')
-export const lightBinarLogo = require('@/assets/logo/logo-light.png')
+export const darkBinarLogo = require('~/assets/logo/logo-dark.png')
+export const lightBinarLogo = require('~/assets/logo/logo-light.png')
 
-export const darkLogoSygnet = require('@/assets/logo/logo-sygnet-dark.png')
-export const lightLogoSygnet = require('@/assets/logo/logo-sygnet-light.png')
+export const darkLogoSygnet = require('~/assets/logo/logo-sygnet-dark.png')
+export const lightLogoSygnet = require('~/assets/logo/logo-sygnet-light.png')
 
-export const googleIcon = require('@/assets/social/google-icon.png')
+export const googleIcon = require('~/assets/social/google-icon.png')
