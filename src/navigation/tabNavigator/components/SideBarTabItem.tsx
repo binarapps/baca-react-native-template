46060db43b9a79c3a4f1ee834921c62f402a84e7
Mateusz Rostkowski <mateusz.rostkowsky995@gmail.com>
2024-10-06 09:42:51 +0200

    refactor: SideBarTabItem component support internationalization and improve code readability

--- src/navigation/tabNavigator/components/SideBarTabItem.tsx
+++ src/navigation/tabNavigator/components/SideBarTabItem.tsx
@@ -5,7 +5,8 @@ import { useUniversalWidth } from '../hooks'
 import { getTabColor } from '../navigation-config'
 
 import { Icon, Row, Text } from '@/design-system'
-import { IconNames } from '@/types'
+import { useTranslation } from '@/hooks'
+import { I18nKeys, IconNames } from '@/types'
 import { makeBigerOnHover } from '@/utils/webStyling'
 
 export function SideBarTabItem({
@@ -15,15 +16,20 @@ export function SideBarTabItem({
   name,
   onPress,
   params,
+  displayedNameTx,
 }: {
-  children: string
+  children?: string
   icon: IconNames
   iconFocused: IconNames
   name: string
   onPress?(): void
   params?: Record<string, string>
+  displayedNameTx?: I18nKeys
 }) {
   const isLarge = useUniversalWidth(1264)
+  const { t } = useTranslation()
+
+  const textToDisplay = displayedNameTx ? t(displayedNameTx) : children
 
   return (
     <TabBarItemWrapper {...{ name, onPress, params }} id={name} style={jsStyles.sidebarTabItem}>
@@ -37,8 +43,8 @@ export function SideBarTabItem({
           style={makeBigerOnHover(hovered)}
         >
           <Icon color={getTabColor(focused)} name={focused ? iconFocused : icon} size={30} />
-          {isLarge ? (
-            <Text.MdSemibold color={getTabColor(focused)}>{children}</Text.MdSemibold>
+          {isLarge && textToDisplay ? (
+            <Text.MdSemibold color={getTabColor(focused)}>{textToDisplay}</Text.MdSemibold>
           ) : null}
         </Row>
       )}
