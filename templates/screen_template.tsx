acdfd9ef5a43d045212b63bde5bd8dd6f9a58238
Mateusz Rostkowski <mateusz.rostkowsky995@gmail.com>
2024-10-08 18:22:55 +0200

    chore: update screen template

--- templates/screen_template.tsx
+++ templates/screen_template.tsx
@@ -1,13 +1,26 @@
+import { useGlobalSearchParams, useLocalSearchParams } from 'expo-router'
+
 import { Center, Text } from '@/design-system'
 import { useScreenOptions, useTranslation } from '@/hooks'
 
 export const _NAME_ = (): JSX.Element => {
+  // Screen params
+  // To use parameters check this docs: https://docs.expo.dev/router/reference/url-parameters/
+  const glob = useGlobalSearchParams()
+  const local = useLocalSearchParams()
+
+  console.log('Local Params:', local.user)
+  console.log('Global Params:', glob.user)
+
+  // Getting translation function
   const { t } = useTranslation()
 
+  // Setting screen title
   useScreenOptions({
     title: '_NAME_',
   })
 
+  // Render
   return (
     <Center flex={1}>
       <Text textAlign="center">_NAME_</Text>
