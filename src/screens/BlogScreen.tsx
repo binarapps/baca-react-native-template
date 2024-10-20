c7ec5af685b9a26c875547056b3f80def15624b5
Mateusz Rostkowski <mateusz.rostkowsky995@gmail.com>
2024-10-20 17:09:33 +0200

    chore: remove unused LandingHeader from BlogScreen

--- src/screens/BlogScreen.tsx
+++ src/screens/BlogScreen.tsx
@@ -1,13 +1,9 @@
-import { LandingHeader } from '@/components/LandingHeader'
 import { Center, Text } from '@/design-system'
 
 export const BlogScreen = () => {
   return (
-    <>
-      <LandingHeader />
-      <Center flex={1} px={4}>
-        <Text>This is blog screen, and is visible by logged in and logged out users</Text>
-      </Center>
-    </>
+    <Center flex={1} px={4}>
+      <Text>This is blog screen, and is visible by logged in and logged out users</Text>
+    </Center>
   )
 }
