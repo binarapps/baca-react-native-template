3002d3640c8a694bf7b1ec255c73b317e9c01e57
Mateusz Rostkowski <mateusz.rostkowsky995@gmail.com>
2024-10-20 17:08:12 +0200

    chore: update navigation on examples screen

--- src/screens/ExamplesScreen.tsx
+++ src/screens/ExamplesScreen.tsx
@@ -11,20 +11,24 @@ export const ExamplesScreen = () => {
     title: t('navigation.screen_titles.examples'),
   })
 
+  // App logic
   const goToApplicationInfo = useCallback(() => push('/application-info'), [push])
-  const goToColors = useCallback(() => push('/example/colors'), [push])
-  const goToComponents = useCallback(() => push('/example/components'), [push])
-  const goToTypography = useCallback(() => push('/example/typography'), [push])
-  const goToStylesBenchmark = useCallback(() => push('/example/style-benchmark'), [push])
-  const goToCityListScreen_EXAMPLE = useCallback(() => push('/example/data-from-be'), [push])
-  const goToTestForm = useCallback(() => push('/example/test-form'), [push])
   const goToPushNotificationsHelpers = useCallback(
     () => push('/example/push-notifications-helpers'),
     [push]
   )
   const goToUserSession = useCallback(() => push('/example/user-session'), [push])
 
+  // UI Examples
+  const goToColors = useCallback(() => push('/colors'), [push])
+  const goToComponents = useCallback(() => push('/components'), [push])
+  const goToTypography = useCallback(() => push('/typography'), [push])
+  const goToStylesBenchmark = useCallback(() => push('/style-benchmark'), [push])
+  const goToTestForm = useCallback(() => push('/test-form'), [push])
+
+  // Others
   const goToHomeStackDetails = useCallback(() => push('/home/details'), [push])
+  const goToCityListScreen_EXAMPLE = useCallback(() => push('/example/data-from-be'), [push])
 
   return (
     <ScrollView p={4} gap={4}>
@@ -38,15 +42,15 @@ export const ExamplesScreen = () => {
       <Button onPress={goToComponents}>{t('examples_screen.go_to_components')}</Button>
       <Button onPress={goToTypography}>{t('examples_screen.go_to_typography')}</Button>
       <Button onPress={goToStylesBenchmark}>{t('examples_screen.go_to_style_benchmark')}</Button>
+      <Button onPress={goToTestForm}>{t('examples_screen.go_to_screen_test_form')}</Button>
 
-      <Display.SmBold>Navigation</Display.SmBold>
+      <Display.SmBold>Others</Display.SmBold>
       <Button onPress={goToHomeStackDetails}>
         {t('examples_screen.go_to_home_stack_details')}
       </Button>
       <Button onPress={goToCityListScreen_EXAMPLE}>
         {t('examples_screen.go_to_screen_with_BEdata')}
       </Button>
-      <Button onPress={goToTestForm}>{t('examples_screen.go_to_screen_test_form')}</Button>
     </ScrollView>
   )
 }
