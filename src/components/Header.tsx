c85120bb8a1d11a1186a369dfa37c72848d36187
Mateusz Rostkowski <38006370+MateuszRostkowski@users.noreply.github.com>
2024-12-31 08:59:47 +0100

    chore: refactor Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,29 +3,23 @@ import { useRouter } from 'expo-router'
 
 import { Box, Column, Row, Icon, Text, Touchable } from '@/design-system'
 
-const logoHeight = 24
-
 export const Header = ({ options }: NativeStackHeaderProps) => {
   const router = useRouter()
+  const showGoBackButton = router.canGoBack()
 
   return (
-    <Column bg="fg.white">
-      <Row bg="fg.white" zIndex={3} justifyContent="space-between" alignItems="center">
+    <Column py={6}>
+      <Row zIndex={3} justifyContent="space-between" alignItems="center">
         <Box ml={4} width={24}>
-          {router.canGoBack() && (
+          {showGoBackButton && (
             <Touchable onPress={router.back}>
-              <Icon name="arrow-left-line" size={24} color="text.brand.primary" />
+              <Icon name="arrow-left-line" size={24} />
             </Touchable>
           )}
         </Box>
         {options.title ? (
           <>
-            <Box height={logoHeight} />
-            <Row ml={8} flex={1}>
-              <Text.LgBold numberOfLines={1} color="text.brand.primary">
-                {options.title}
-              </Text.LgBold>
-            </Row>
+            <Text.LgBold numberOfLines={1}>{options.title}</Text.LgBold>
           </>
         ) : (
           <Box />
