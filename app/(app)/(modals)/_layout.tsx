6392e99f1108fa2fec668f2405b6738a8b7de7a8
Mateusz Rostkowski <38006370+MateuszRostkowski@users.noreply.github.com>
2024-12-31 09:03:08 +0100

    chore: change header for modals route

--- app/(app)/(modals)/_layout.tsx
+++ app/(app)/(modals)/_layout.tsx
@@ -1,9 +1,18 @@
 import { Stack } from 'expo-router'
 
+import { Header } from '@/components/Header'
+
 export const unstable_settings = {
   initialRouteName: 'application-info',
 }
 
 export default function ModalsLayout() {
-  return <Stack />
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: true,
+        header: Header,
+      }}
+    />
+  )
 }
