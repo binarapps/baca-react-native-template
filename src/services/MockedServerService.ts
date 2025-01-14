52c2f76d4ef22e78f5c99c4eb55bb209f68c5cf2
Mateusz Rostkowski <mateusz.rostkowsky995@gmail.com>
2025-01-14 19:37:43 +0100

    chore: comment out unused imports and functions in MockedServerService

--- src/services/MockedServerService.ts
+++ src/services/MockedServerService.ts
@@ -1,23 +1,27 @@
-import 'text-encoding-polyfill'
-import 'fast-text-encoding'
-import 'react-native-url-polyfill/auto'
+// FIXME: Fix the issue with the msw/native package
+// - https://github.com/mswjs/msw/issues/2344
+// - https://github.com/mswjs/msw/issues/2367
 
-import { setupServer } from 'msw/native'
+// import 'text-encoding-polyfill'
+// import 'fast-text-encoding'
+// import 'react-native-url-polyfill/auto'
 
-import { getArticlesMock } from '@/api/query/articles/articles.msw'
-import { getAuthMock } from '@/api/query/auth/auth.msw'
-import { getAuthSocialMock } from '@/api/query/auth-social/auth-social.msw'
-import { getFilesMock } from '@/api/query/files/files.msw'
-import { getUsersMock } from '@/api/query/users/users.msw'
+// import { setupServer } from 'msw/native'
 
-export const startMockedServer = (): void => {
-  const server = setupServer(
-    ...getAuthMock(),
-    ...getUsersMock(),
-    ...getFilesMock(),
-    ...getAuthSocialMock(),
-    ...getArticlesMock()
-  )
+// import { getArticlesMock } from '@/api/query/articles/articles.msw'
+// import { getAuthMock } from '@/api/query/auth/auth.msw'
+// import { getAuthSocialMock } from '@/api/query/auth-social/auth-social.msw'
+// import { getFilesMock } from '@/api/query/files/files.msw'
+// import { getUsersMock } from '@/api/query/users/users.msw'
 
-  server.listen({ onUnhandledRequest: 'bypass' })
-}
+// export const startMockedServer = (): void => {
+//   const server = setupServer(
+//     ...getAuthMock(),
+//     ...getUsersMock(),
+//     ...getFilesMock(),
+//     ...getAuthSocialMock(),
+//     ...getArticlesMock()
+//   )
+
+//   server.listen({ onUnhandledRequest: 'bypass' })
+// }
