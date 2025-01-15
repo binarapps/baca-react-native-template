9a8349525e9a36deedf0374a144aa68cee6ec29b
Mateusz Rostkowski <mateusz.rostkowsky995@gmail.com>
2025-01-15 15:09:20 +0100

    chore: update header back button display mode to 'minimal' in layout components

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -6,7 +6,7 @@ export const unstable_settings = {
 
 export default function AppLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false, headerBackTitleVisible: false }}>
+    <Stack screenOptions={{ headerShown: false, headerBackButtonDisplayMode: 'minimal' }}>
       <Stack.Screen name="(authorized)" />
       <Stack.Screen name="(not-authorized)" />
       <Stack.Screen name="(common)" />
